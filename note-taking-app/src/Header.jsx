2052f76172e134f44fe719a6335439435a1dc719
Shaked Yunger <shakedyunger@gmail.com>
2022-05-28 21:30:00 +0300

    create new branch

--- note-taking-app/src/Header.jsx
+++ note-taking-app/src/Header.jsx
@@ -4,7 +4,7 @@ export class Header extends Component {
   render() {
     return (
       <div className="header">
-        {/* <h1>Note-Taking-App</h1> */}
+        <h1>Note-Taking-App</h1>
         <button type="button" class="primaryButton">
           Add note
         </button>
